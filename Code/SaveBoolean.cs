dc46f03324409e71116aab73ace4373f89bb9821
Gold <88539712+Goldhahaha@users.noreply.github.com>
2021-11-03 15:32:39 +0800

    Update SaveBoolean.cs

--- Code/SaveBoolean.cs
+++ Code/SaveBoolean.cs
@@ -1,5 +1,5 @@
-/*Set Method*/
-private static void set_true_and_true(string boolean_stirng_key, bool true_or_false)
+    /*Set Method*/
+    private static void set_true_and_true(string boolean_stirng_key, bool true_or_false)
     {
         if (!true_or_false)
         {
@@ -10,8 +10,8 @@ private static void set_true_and_true(string boolean_stirng_key, bool true_or_fa
             PlayerPrefs.SetInt(boolean_stirng_key, 1);
         }
     }
-/*Get Method*/
-private static bool get_true_or_false(string boolean_stirng_key)
+    /*Get Method*/
+    private static bool get_true_or_false(string boolean_stirng_key)
     {
         if (PlayerPrefs.GetInt(boolean_stirng_key) == 0)
         {

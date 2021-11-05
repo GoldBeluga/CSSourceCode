9a62cb9857320fd20803dbe22225a6837ce9294e
Gold <88539712+Goldhahaha@users.noreply.github.com>
2021-11-05 18:35:09 +0800

    Update SaveBoolean.cs

--- Code/SaveBoolean.cs
+++ Code/SaveBoolean.cs
@@ -1,4 +1,4 @@
-    /*Set Method*/
+    /*Set*/
     private static void set_true_or_false(string boolean_stirng_key, bool true_or_false)
     {
         if (!true_or_false)
@@ -10,15 +10,5 @@
             PlayerPrefs.SetInt(boolean_stirng_key, 1);
         }
     }
-    /*Get Method*/
-    private static bool get_true_or_false(string boolean_stirng_key)
-    {
-        if (PlayerPrefs.GetInt(boolean_stirng_key) == 0)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
+    /*Get*/
+    private static bool get_true_or_false(string boolean_stirng_key) => PlayerPrefs.GetInt(boolean_stirng_key) == 1;

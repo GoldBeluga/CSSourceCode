276989fa8b91ead9984bec060ba758c96028e202
Gold <88539712+Goldhahaha@users.noreply.github.com>
2021-11-10 16:57:53 +0800

    Create Shop.cs

---
+++ Code/Shop.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Shop : MonoBehaviour
+{
+    void not_enough(Text not_enough_text, int hide_not_enough_time)
+    {
+        not_enough_text.gameObject.SetActive(true);
+        StartCoroutine(wait(hide_not_enough_time, not_enough_text));
+    }
+
+    IEnumerator wait(int sec, Text hide_not_enough_time)
+    {
+        yield return new WaitForSeconds(sec);
+        hide_not_enough_time.gameObject.SetActive(false);
+    }
+
+    public void player_shop_calculate(ref float player_property, float player_get_value, float player_give_value, float player_minimum_value, Text not_enough_text, int hide_not_enough_time)
+    {
+        not_enough_text.gameObject.SetActive(false);
+        if (player_property >= player_minimum_value)
+        {
+            player_property -= player_give_value;
+            player_property += player_get_value;
+        }
+        else if (player_property < player_minimum_value)
+        {
+            not_enough(not_enough_text, hide_not_enough_time);
+        }
+    }
+
+    public void player_shop_calculate(ref float player_property, ref float player_second_property, float player_get_value, float player_give_value, float player_minimum_value, Text not_enough_text, int hide_not_enough_time)
+    {
+        not_enough_text.gameObject.SetActive(false);
+        if (player_property >= player_minimum_value)
+        {
+            player_property -= player_give_value;
+            player_second_property += player_get_value;
+        }
+        else if (player_property < player_minimum_value)
+        {
+            not_enough(not_enough_text, hide_not_enough_time);
+        }
+
+    }
+}
+

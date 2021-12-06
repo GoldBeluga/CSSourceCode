51c393c6748a02a89f423b091d43ad3ae7f9c7a5
Gold <88539712+Goldhahaha@users.noreply.github.com>
2021-12-06 12:18:09 +0800

    Update SaveBoolean.cs

--- Code/SaveBoolean.cs
+++ Code/SaveBoolean.cs
@@ -1,14 +1,7 @@
     /*Set*/
     private static void set_boolean(string key, bool value)
-    {
-        if (!value)
-        {
-            PlayerPrefs.SetInt(key, 0);
-        }
-        if (value)
-        {
-            PlayerPrefs.SetInt(key, 1);
-        }
+    {      
+            PlayerPrefs.SetInt(key, value ? 1 : 0);
     }
     /*Get*/
     private static bool get_boolean(string key) => PlayerPrefs.GetInt(key) == 1;

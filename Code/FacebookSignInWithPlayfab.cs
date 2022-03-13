2a73e59235eaa1bb231e28f32b01b187a7d4e2f4
Gold <88539712+Goldhahaha@users.noreply.github.com>
2022-03-13 08:09:54 +0800

    Create FacebookSignInWithPlayfab.cs

---
+++ Code/FacebookSignInWithPlayfab.cs
@@ -0,0 +1,37 @@
+public class FacebookSignInWithPlayfab
+{
+  void sign_in_with_facebook()
+    {
+        FB.Init(() =>
+        {
+            if (FB.IsLoggedIn)
+            {
+                FB.LogOut();
+            }
+            FB.LogInWithReadPermissions(null, internal_sign_in_with_facebook =>
+            {
+                if (internal_sign_in_with_facebook == null || string.IsNullOrEmpty(internal_sign_in_with_facebook.Error))
+                {
+                    Debug.Log("Facebook Auth Complete! Access Token: " + AccessToken.CurrentAccessToken.TokenString + "\nLogging into PlayFab...");
+
+                    PlayFabClientAPI.LoginWithFacebook(new LoginWithFacebookRequest
+                    {
+                        CreateAccount = true,
+                        AccessToken = AccessToken.CurrentAccessToken.TokenString
+                    },
+                        OnPlayfabFacebookAuthComplete =>
+                        {
+                            Debug.Log("PlayFab Facebook Auth Complete. Session ticket: " + OnPlayfabFacebookAuthComplete.SessionTicket);
+                        }, OnPlayfabFacebookAuthFailed =>
+                        {
+                            Debug.LogWarning(OnPlayfabFacebookAuthFailed.GenerateErrorReport());
+                        });
+                }
+                else
+                {
+                    Debug.LogError("Facebook Auth Failed: " + internal_sign_in_with_facebook.Error + "\n" + internal_sign_in_with_facebook.RawResult);
+                }
+            });
+        });
+    }
+}
